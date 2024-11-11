dff6e3a92ef438bc87d6153312d596d61ebb4656
tonmoysutradhar <tonmoykumar933@gmail.com>
2024-11-12 00:57:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import Posts from './Posts'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>React Core Concepts Recap</h1>
+      <ol>
+        <li>Components</li>
+        <li>JSX</li>
+        <li>Props</li>
+        <li>Event Handler</li>
+        <li>State</li>
+        <li>Loas Data</li>
+      </ol>
+      <hr />
+      <Posts></Posts>
+    </>
+  )
+}
+
+export default App
